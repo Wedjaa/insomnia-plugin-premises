999064a56aedf83d49f070f445d72431ef77aeff
Fabio Torchetti <fabio.torchetti@accenture.com>
2020-04-17 07:59:15 -0500

    Initial Release

---
+++ iso7064.js
@@ -0,0 +1,332 @@
+class PureSystemCalculator {
+    constructor({
+                    Modulus,
+                    Radix,
+                    Remainder = 1,
+                    ApplicationCharset,
+                    CheckCharset,
+                    IsDoubleCheckCharacter,
+                    SingleDigitDesignation = 0,
+                    IsCaseSensitive = false
+                }) {
+        this.M = Modulus;
+        this.r = Radix;
+        this.acs = ApplicationCharset;
+        this.ccs = CheckCharset;
+        this.dblchk = IsDoubleCheckCharacter;
+        this.R = Remainder;
+        this.desig = SingleDigitDesignation;
+        this.cs = IsCaseSensitive;
+
+        this.patt_a = new RegExp(`^[${this.acs}]+$`, this.cs ? null : "i")
+
+        this.acsEnum = {};
+        for (let i = 0; i < this.acs.length; i++)
+            this.acsEnum[this.acs[i]] = i;
+        Object.freeze(this.acsEnum);
+    }
+
+    //Returns the computed check character(s) only
+    //If input type is invalid or input string is empty, return null;
+    //If input string contains character outside charset, return undefined;
+    compute(input) {
+        if (typeof input !== "string" || input === "")
+            return null;
+
+        if (!this.patt_a.test(input))
+            return undefined;
+
+        if (!this.cs) input = input.toUpperCase();
+
+        let P = 0;
+        for (let i = 0; i < input.length; i++) {
+            let a = this.acsEnum[input.charAt(i)];
+            P = ((P + a) * this.r) % this.M;
+        }
+        if (this.dblchk) {
+            P = (P * this.r) % this.M;
+            let V = this.M + this.R - P;
+            let quotient = ~~(V / this.r);
+            let remainder = V - quotient * this.r;
+            return this.ccs.charAt(quotient) + this.ccs.charAt(remainder);
+        } else {
+            return this.ccs.charAt((this.M + this.R - P) % this.M);
+        }
+    }
+
+    //Returns the full string with check character(s)
+    complete(input) {
+        let checkbit = this.compute(input);
+        if (typeof checkbit === "string")
+            return input + checkbit;
+        else
+            return checkbit;
+    }
+
+    //Verify the full string
+    verify(input) {
+        let checkLen = 1 + this.dblchk;
+        if (typeof input !== "string" || input.length <= checkLen)
+            return null;
+        let dataOnly = input.substr(0, input.length - checkLen);
+        let checkbit = this.compute(dataOnly);
+        if (typeof checkbit === "string")
+            return checkbit === (this.cs ? input.substr(-checkLen) : input.substr(-checkLen).toUpperCase());
+        else
+            return checkbit;
+    }
+}
+
+//The abstract class of hybrid system
+class HybridSystemCalculator extends PureSystemCalculator {
+    constructor({
+                    Modulus,
+                    Remainder,
+                    ApplicationCharset,
+                    CheckCharset,
+                    SingleDigitDesignation,
+                    IsCaseSensitive
+                }) {
+        super({
+            Modulus: Modulus,
+            Radix: 2,
+            Remainder: Remainder,
+            ApplicationCharset: ApplicationCharset,
+            CheckCharset: CheckCharset,
+            IsDoubleCheckCharacter: false,
+            SingleDigitDesignation: SingleDigitDesignation,
+            IsCaseSensitive: IsCaseSensitive
+        });
+    }
+
+    compute(input) {
+        if (typeof input !== "string" || input === "")
+            return null;
+
+        if (!this.patt_a.test(input))
+            return undefined;
+
+        if (!this.cs) input = input.toUpperCase();
+
+        let P = this.M;
+        for (let i = 0; i < input.length; i++) {
+            let a = this.acsEnum[input.charAt(i)];
+            P = (P + a) % this.M;
+            if (P === 0)
+                P = this.M;
+            P = (P * this.r) % (this.M + 1);
+        }
+        return this.ccs.charAt((this.M + this.R - P) % this.M);
+    }
+}
+
+
+//Here comes actually usable classes
+class MOD11_2 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 11,
+            Radix: 2,
+            ApplicationCharset: "0123456789",
+            CheckCharset: "0123456789X",
+            IsDoubleCheckCharacter: false,
+            SingleDigitDesignation: 1
+        });
+
+        this.patt_fast = new RegExp(/^\d+[\dX]$/, this.cs ? null : "i");
+
+        this.ccsEnum = { "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "X": 10 };
+    }
+
+    //Faster on Chrome & Edge; slower on Firefox
+    verify_fast(input) {
+        if (typeof input !== "string" || input.length <= 1)
+            return null;
+
+        if (!this.patt_fast.test(input))
+            return undefined;
+
+        let P = 0;
+        for (let i = 0; i < input.length - 1; i++) {
+            P += +input.charAt(i);
+            P <<= 1;
+        }
+        P += this.ccsEnum[input.charAt(input.length - 1)];
+        return P % 11 === 1;
+    }
+}
+
+class MOD37_2 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 37,
+            Radix: 2,
+            ApplicationCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            CheckCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*",
+            IsDoubleCheckCharacter: false,
+            SingleDigitDesignation: 2
+        });
+    }
+}
+
+class MOD97_10 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 97,
+            Radix: 10,
+            ApplicationCharset: "0123456789",
+            CheckCharset: "0123456789",
+            IsDoubleCheckCharacter: true,
+            SingleDigitDesignation: 3
+        });
+        this.patt_fast = new RegExp(/^\d+$/);
+    }
+
+    //Simplified procedure for ISO/IEC 7064, MOD 97–10
+    compute_fast(input) {
+        if (typeof input !== "string" || input === "")
+            return null;
+
+        if (!this.patt_fast.test(input))
+            return undefined;
+
+        return `${98 - input * 100 % 97}`;
+    }
+    complete_fast(input) {
+        let checkbit = this.compute_fast(input);
+        if (typeof checkbit === "string")
+            return input + checkbit;
+        else
+            return checkbit;
+    }
+    verify_fast(input) {
+        if (typeof input !== "string" || input.length <= 2)
+            return null;
+
+        if (!this.patt_fast.test(input))
+            return undefined;
+
+        return input % 97 === 1;
+    }
+}
+
+class MOD661_26 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 661,
+            Radix: 26,
+            ApplicationCharset: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            CheckCharset: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            IsDoubleCheckCharacter: true,
+            SingleDigitDesignation: 4
+        });
+    }
+}
+
+class MOD1271_36 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 1271,
+            Radix: 36,
+            ApplicationCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            CheckCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            IsDoubleCheckCharacter: true,
+            SingleDigitDesignation: 5
+        });
+    }
+}
+
+class MOD11_10 extends HybridSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 10,
+            ApplicationCharset: "0123456789",
+            CheckCharset: "0123456789",
+            SingleDigitDesignation: 6
+        });
+    }
+}
+
+class MOD27_26 extends HybridSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 26,
+            ApplicationCharset: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            CheckCharset: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            SingleDigitDesignation: 7
+        });
+    }
+}
+
+class MOD37_36 extends HybridSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 36,
+            ApplicationCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            CheckCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            SingleDigitDesignation: 8
+        });
+    }
+}
+
+
+//GB 11643-1999
+//https://zh.wikisource.org/wiki/GB_11643-1999_公民身份号码
+class GB11643 extends MOD11_2 {
+    //Basically the same as MOD 11-2
+}
+
+//GB 11714-1997
+//https://zh.wikisource.org/wiki/GB_11714-1997_全国组织机构代码编制规则
+class GB11714 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 11,
+            Radix: 2,
+            Remainder: 0,
+            ApplicationCharset: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            CheckCharset: "0123456789X",
+            IsDoubleCheckCharacter: false
+        });
+    }
+}
+
+//GB 32100-2015
+//https://zh.wikisource.org/wiki/GB_32100-2015_法人和其他组织统一社会信用代码编码规则
+class GB32100 extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 31,
+            Radix: 3,
+            ApplicationCharset: "0123456789ABCDEFGHJKLMNPQRTUWXY",
+            CheckCharset: "0123456789ABCDEFGHJKLMNPQRTUWXY",
+            IsDoubleCheckCharacter: false,
+            Remainder: 0
+        });
+    }
+    compute(input) {
+        let P = 0;
+        for (let i = input.length - 1; i >= 0; i--) {
+            let a = this.acsEnum[input.charAt(i)];
+            P = ((P * this.r) % this.M + a);
+        }
+        return this.ccs.charAt((this.M + this.R - P) % this.M);
+    }
+}
+
+//Example on extension: MOD 29-2 (recursive) on Danish alphabet (see Annex B of ISO/IEC 7064)
+class MOD29_2_DK extends PureSystemCalculator {
+    constructor() {
+        super({
+            Modulus: 29,
+            Radix: 2,
+            ApplicationCharset: "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ",
+            CheckCharset: "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ",
+            IsDoubleCheckCharacter: false
+        });
+    }
+}
+
+module.exports = {
+    MOD37_36
+};
---
+++ premises.js
@@ -0,0 +1,158 @@
+const Chance = require('chance');
+const randomPoint = require('@turf/random').randomPoint;
+const booleanPointInPolygon = require('@turf/boolean-point-in-polygon').default;
+const States = require('./states.json');
+const StateBounds = require('./state-bounds.json');
+const chance = new Chance();
+const path = require('path');
+const {MOD37_36} = require('./iso7064');
+let check = new MOD37_36();
+
+const parse = require('csv-parse/lib/sync');
+
+const fs = require('fs');
+
+const loadCsv = (filename) => {
+    const options = {
+        columns: true,
+        skip_empty_lines: true
+    };
+    return parse(fs.readFileSync(path.join(__dirname, filename), 'utf-8'), options);
+};
+
+const capitalize = (word) => {
+    return  word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+};
+
+const pigs = loadCsv('./pigs.csv');
+const farms = loadCsv('./farms.csv');
+const adjectives = loadCsv('./adjectives.csv');
+
+const pickRandom = (list) => {
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+
+const makeId = (length) => {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const premiseId = () => {
+    let base = makeId(2);
+    let num = `000${Math.floor(9999 * Math.random())}`.substr(-4);
+    return `${num}${base}${check.compute(num + base)}`
+}
+
+const randomName = () => {
+    const firstName = capitalize(pickRandom(firstNames).name);
+    const lastName =  capitalize(pickRandom(surnames).name);
+    return `${firstName} ${lastName}`;
+}
+
+const randomFarm = () => {
+    const adj = capitalize(pickRandom(adjectives)['adjective']);
+    const pork = capitalize(pickRandom(pigs)['synonym']);
+    const farm = capitalize(pickRandom(farms)['synonym']);
+    return `${adj} ${pork} ${farm}`;
+}
+
+const makeSafe = (words) => {
+    return  words
+        .toLowerCase()
+        .replace(/\s/g, '.')
+        .replace(/[^a-z]/g, '.')
+        .replace(/\.+/, '.');
+}
+
+const domain = (producer) => {
+    const roots = [ 'com', 'pork', 'org', 'net' ];
+
+    return `${makeSafe(producer)}.${pickRandom(roots)}`;
+}
+
+const email = (user, producer) => {
+    return `${makeSafe(user)}@${domain(producer)}`;
+}
+
+const randomLocation = (bounds, shape) => {
+    
+    const point = randomPoint(1, {bbox: [
+        bounds.min_lng, 
+        bounds.min_lat, 
+        bounds.max_lng, 
+        bounds.max_lat
+    ]}).features[0];
+
+    const coords = {
+        lat: point.geometry.coordinates[1],
+        lng: point.geometry.coordinates[0],
+    }
+
+    return booleanPointInPolygon(point, shape) ? coords : randomLocation(bounds, shape);
+};
+
+const randomPremise = (state, bounds, shape) => {
+
+    const iceName = chance.name();
+    const producerName = randomFarm();
+    const barns = Math.floor(Math.random() * 50);
+    const animals = barns * Math.floor(Math.random() * (250 - 50 + 1)) + 50;
+    const present = animals - Math.floor(Math.random() * (animals - 10 + 1)) + 10;
+    const location = randomLocation(bounds, shape);
+    return {
+        premiseId: premiseId(),
+        producer: producerName,
+        premName: randomFarm(),
+        species: 'swine',
+        iceContactName: iceName,
+        state,
+        zip: chance.zip(),
+        city: chance.city(),
+        streetAddress: chance.address({short_suffix: true}),
+        locationType: pickRandom(["Grower", "Finisher", "Weaner", "Nursery"]),
+        latitude: location.lat,
+        longitude: location.lng,
+        iceContactPhone: chance.phone(),
+        iceContactEmail: email(iceName, producerName),
+        siteCapacityNumberBarns: barns,
+        siteCapacityNumberAnimals: animals,
+        numberOfAnimalsOnSite: present
+    }
+
+}
+
+module.exports.templateTags = [{
+    name: 'premises',
+    displayName: 'Random Premises',
+    description: 'Generate random premises',
+    args: [
+        {
+            displayName: 'Number Of Premises',
+            description: 'Number Of Premises to Generate',
+            type: 'number',
+            defaultValue: 10
+        }, 
+        {
+            displayName: 'State',
+            description: 'State To Create Premises For',
+            type: 'enum',
+            defaultValue: 'IA',
+            options: Object.keys(States).map(stateCode =>({
+                displayName: States[stateCode].name,
+                value: stateCode,
+            })),
+        }
+    ],
+    async run (context, number, state) {
+        const stateBounds = States[state.toUpperCase()]
+        const stateShape = StateBounds.features.find(feature => feature.properties.STUSPS === state);
+
+        return JSON.stringify([...Array(number).keys()].map( idx => randomPremise(state, stateBounds, stateShape)), undefined, 4);
+    }
+}];
